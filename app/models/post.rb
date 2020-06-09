f785c58b7d7375ce5ed3c3a269ce0305b062527f
toshihiro523montreal <toshihiro523montreal@gmail.comquitexitgit config --global user.name toshihiro523montreal>
2020-06-09 16:32:18 +0900

    implement attachment count validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,14 @@ class Post < ApplicationRecord
   validates :title, length: { maximum: 32 }, presence: true
   
   validate :validate_content_attachment_byte_size
-  validate :validate_content_length
+  validate :validate_content_attachments_count
+  validate :validate_content_length  
 
   MAX_CONTENT_LENGTH = 50
   ONE_KILOBYTE = 1024
-  MEGA_BYTES = 3
+  MEGA_BYTES = 4
   MAX_CONTENT_ATTACHMENT_BYTE_SIZE = MEGA_BYTES * 1_000 * ONE_KILOBYTE
+  MAX_CONTENT_ATTACHMENT_COUNT = 4
 
   private
   
@@ -23,6 +25,17 @@ class Post < ApplicationRecord
         )
       end
     end
+  end
+
+  def validate_content_attachments_count
+    if content.body.attachables.grep(ActiveStorage::Blob).count > MAX_CONTENT_ATTACHMENT_COUNT
+      errors.add(
+        :content,
+        :attachments_count_is_too_big,
+        max_content_attachments_count: MAX_CONTENT_ATTACHMENT_COUNT
+      )
+    end
+  
   end
   
   def validate_content_length
